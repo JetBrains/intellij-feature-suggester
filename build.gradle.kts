9ec9d5e9c08b8f97ac69a05a8df3fbb79497aa37
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-03-10 12:56:55 +0100

    Migrate Gradle build file to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,50 @@
+import java.io.ByteArrayOutputStream
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    id 'java'
-    id 'org.jetbrains.intellij' version '0.7.2'
-    id 'org.jetbrains.kotlin.jvm' version '1.3.72'
+    id("java")
+    id("org.jetbrains.intellij") version "0.7.2"
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
 }
 
-def getGitHash = { ->
-    def stdout = new ByteArrayOutputStream()
+fun getGitHash(): String {
+    val stdout = ByteArrayOutputStream()
     exec {
-        commandLine 'git', 'rev-list', '--count', 'HEAD'
+        commandLine("git", "rev-list", "--count", "HEAD")
         standardOutput = stdout
     }
     return stdout.toString().trim()
 }
 
-group 'org.jetbrains'
+group = "org.jetbrains"
 version "211.${getGitHash()}"
-
-sourceCompatibility = 11
-targetCompatibility = 11
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation "org.jetbrains.kotlin:kotlin-stdlib-jdk8"
-    testCompile group: 'junit', name: 'junit', version: '4.12'
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testCompile("junit:junit:4.12")
 }
 
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
-    version 'IU-211-EAP-SNAPSHOT'
-    plugins 'java', 'Kotlin', 'JavaScript', 'Pythonid:211.6222.4'
+    version = "IU-211-EAP-SNAPSHOT"
+    setPlugins("java", "Kotlin", "JavaScript", "Pythonid:211.6222.4")
 }
-compileKotlin {
-    kotlinOptions.jvmTarget = "11"
-}
-compileTestKotlin {
-    kotlinOptions.jvmTarget = "11"
-}
-patchPluginXml {
-    sinceBuild = "211"
-    changeNotes """
-      Add change notes here.<br>
-      <em>most HTML tags may be used</em>"""
+
+tasks {
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild("211")
+        changeNotes("Add change notes here.<br><em>most HTML tags may be used</em>")
+    }
 }
